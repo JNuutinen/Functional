b9e5cfbf19abdfd5c3518ef3bc3dbacc7e9a1d44
Juha Nuutinen <juha.nuutinen@protonmail.com>
2018-12-09 07:15:58 +0200

    Add more to-do deletion tests.

--- app/src/androidTest/java/com/github/jnuutinen/functional/ui/DeleteTodoTest.kt
+++ app/src/androidTest/java/com/github/jnuutinen/functional/ui/DeleteTodoTest.kt
@@ -1,13 +1,9 @@
 package com.github.jnuutinen.functional.ui
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.swipeLeft
-import androidx.test.espresso.action.ViewActions.swipeRight
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -19,7 +15,7 @@ import com.github.jnuutinen.functional.helper.RecyclerItemCountAssert.Companion.
 import com.github.jnuutinen.functional.helper.RecyclerViewMatcher.Companion.withRecyclerView
 import com.github.jnuutinen.functional.helper.TestDatabaseHelper.Companion.repopulateDb
 import com.github.jnuutinen.functional.helper.TestDatabaseHelper.Companion.setActiveListSharedPref
-import com.github.jnuutinen.functional.helper.UiTestHelper
+import com.github.jnuutinen.functional.helper.UiTestHelper.Companion.openNavigationDrawer
 import com.github.jnuutinen.functional.presentation.activity.TodosActivity
 import org.junit.Before
 import org.junit.Rule
@@ -46,21 +42,25 @@ class DeleteTodoTest {
         repopulateDb()
         mActivityTestRule.launchActivity(null)
         Thread.sleep(1000)
+
+        // Test the setup.
+        val firstTodoName = "First list, first to-do"
+        val secondTodoName = "First list, second to-do"
+        val thirdTodoName = "First list, third to-do"
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
+            .check(matches(hasDescendant(withText(firstTodoName))))
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(secondTodoName))))
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(2))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
     }
 
     @Test
     fun deleteTodo_First() {
-        // Ensure the first and second to-dos are correct.
-        val firstTodoName = "First list, first to-do"
         val secondTodoName = "First list, second to-do"
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
-            .check(ViewAssertions.matches(hasDescendant(withText(firstTodoName))))
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
-            .check(ViewAssertions.matches(hasDescendant(withText(secondTodoName))))
 
         // Delete the first to-do by swiping it.
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
-            .perform(swipeRight())
+        deleteTodo(1)
 
         // The former second to-do should now be the first one.
         onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
@@ -88,17 +88,10 @@ class DeleteTodoTest {
 
     @Test
     fun deleteTodo_Middle() {
-        // Ensure the second (middle) and last (third) to-dos are correct.
-        val secondTodoName = "First list, second to-do"
         val thirdTodoName = "First list, third to-do"
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
-            .check(matches(hasDescendant(withText(secondTodoName))))
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(2))
-            .check(matches(hasDescendant(withText(thirdTodoName))))
 
         // Delete the second to-do by swiping it.
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
-            .perform(swipeLeft())
+        deleteTodo(1)
 
         // The former third to-do should now be the second one.
         onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
@@ -125,17 +118,10 @@ class DeleteTodoTest {
 
     @Test
     fun deleteTodo_Last() {
-        // Ensure the second (middle) and last (third) to-dos are correct.
         val secondTodoName = "First list, second to-do"
-        val thirdTodoName = "First list, third to-do"
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
-            .check(matches(hasDescendant(withText(secondTodoName))))
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(2))
-            .check(matches(hasDescendant(withText(thirdTodoName))))
 
         // Delete the third to-do by swiping it.
-        onView(withRecyclerView(R.id.todo_recycler).atPosition(2))
-            .perform(swipeLeft())
+        deleteTodo(1)
 
         // The former second to-do should still be the second one.
         onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
@@ -158,5 +144,125 @@ class DeleteTodoTest {
 
         onView(withId(R.id.todo_recycler))
             .check(hasSize(2))
+    }
+
+    @Test
+    fun deleteTodo_Middle_InAnotherList() {
+        // Switch to the second test list.
+        openNavigationDrawer(mActivityTestRule.activity)
+        onView(withText("Second to-do list"))
+            .perform(click())
+
+        val thirdTodoName = "Second list, third to-do"
+
+        // Delete the second to-do by swiping it.
+        deleteTodo(1)
+
+        // The former third to-do should now be the second one.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
+
+        // The number of to-dos should now be 2.
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(2))
+
+        // Go back to the first test list.
+        openNavigationDrawer(mActivityTestRule.activity)
+        onView(withText("First to-do list"))
+            .perform(click())
+
+        // The first test list should be unaffected.
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(3))
+
+        // Return to the second list.
+        openNavigationDrawer(mActivityTestRule.activity)
+        onView(withText("Second to-do list"))
+            .perform(click())
+
+        // Make the same checks as before.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
+
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(2))
+    }
+
+    @Test
+    fun deleteTodo_Middle_Undo() {
+        val secondTodoName = "First list, second to-do"
+        val thirdTodoName = "First list, third to-do"
+
+        // Delete the second to-do by swiping it.
+        deleteTodo(1)
+
+        // The former third to-do should now be the second one.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
+
+        // The number of to-dos should now be 2.
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(2))
+
+        Thread.sleep(1000)
+
+        // Undo the deletion.
+        onView(withText("UNDO"))
+            .perform(click())
+
+        // We should now be in the original state.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(secondTodoName))))
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(2))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
+
+        // The number of to-dos should now be 3.
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(3))
+    }
+
+    @Test
+    fun deleteTwoTodos() {
+        val firstTodoName = "First list, first to-do"
+
+        // Delete the second to-do twice to delete the middle and last to-dos.
+        deleteTodo(1)
+        deleteTodo(1)
+
+        // Only the first to-do should remain.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
+            .check(matches(hasDescendant(withText(firstTodoName))))
+
+        // The number of to-dos should now be 1.
+        onView(withId(R.id.todo_recycler))
+            .check(hasSize(1))
+    }
+
+    @Test
+    fun deleteTwoTodos_Undo() {
+        val firstTodoName = "First list, first to-do"
+        val thirdTodoName = "First list, third to-do"
+
+        // Delete the second to-do by swiping it.
+        deleteTodo(1)
+
+        // Delete the first to-do by swiping it.
+        deleteTodo(0)
+
+        // Undo the deletion of the first to-do.
+        onView(withText("UNDO"))
+            .perform(click())
+
+        // The first and third to-do should remain.
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(0))
+            .check(matches(hasDescendant(withText(firstTodoName))))
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(1))
+            .check(matches(hasDescendant(withText(thirdTodoName))))
+    }
+
+    private fun deleteTodo(index: Int) {
+        onView(withRecyclerView(R.id.todo_recycler).atPosition(index))
+            .perform(swipeLeft())
+        Thread.sleep(1000)
     }
 }
